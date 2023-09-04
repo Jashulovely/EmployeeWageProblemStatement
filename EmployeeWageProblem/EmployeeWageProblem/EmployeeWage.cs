3d70adc47cc637f099ab43d465014071d5777526
jashulovely <porsu.jaswanthkumar@gmail.com>
2023-09-04 16:48:12 +0530

    [Jashu] Add: Employee Wage Calculation Using Switch Case Statement.

--- EmployeeWageProblem/EmployeeWageProblem/EmployeeWage.cs
+++ EmployeeWageProblem/EmployeeWageProblem/EmployeeWage.cs
@@ -11,26 +11,23 @@ namespace EmployeeWageProblem
         const int wagePerHour = 20, isFullTime = 1, isPartTime = 2;
         int fullDayHour = 0;
 
-
-
         public void Attendance()
         {
             Random random = new Random();
             int empCheck = random.Next(0,3);
-            if(empCheck == isFullTime)
+
+            switch (empCheck)
             {
-                Console.WriteLine("Employee is present for Full Time.");
-                fullDayHour = 8;
-            }
-            else if ( empCheck == isPartTime)
-            {
-                Console.WriteLine("Employee is present for Part Time.");
-                fullDayHour = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is abscent");
-                fullDayHour = 0;
+                case isFullTime: fullDayHour = 8;
+                    Console.WriteLine("Employee is present for Full Time.");
+                    break;
+
+                case isPartTime: fullDayHour = 4;
+                    Console.WriteLine("Employee is present for Part Time.");
+                    break;
+                default: fullDayHour = 0;
+                    Console.WriteLine("Employee is Abscent.");
+                    break;
             }
             int  empWage = fullDayHour * wagePerHour;
             Console.WriteLine("Daily wage of Employee is : " + empWage);
