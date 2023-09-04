7ce1e4ca624c0cd3b529c2037fdd6400dbd2b159
jashulovely <porsu.jaswanthkumar@gmail.com>
2023-09-04 09:23:51 +0530

    [Jashu] Add: Employee Wage for Present and Abscent

--- EmployeeWageProblem/EmployeeWageProblem/EmployeeWage.cs
+++ EmployeeWageProblem/EmployeeWageProblem/EmployeeWage.cs
@@ -8,18 +8,27 @@ namespace EmployeeWageProblem
 {
     internal class EmployeeWage
     {
+        const int wagePerHour = 20, isFullTime = 1;
+        int fullDayHour = 0;
+
+
+
         public void Attendance()
         {
             Random random = new Random();
             int empCheck = random.Next(0,2);
-            if(empCheck == 0)
+            if(empCheck == isFullTime)
             {
                 Console.WriteLine("Employee is present");
+                fullDayHour = 8;
             }
             else
             {
                 Console.WriteLine("Employee is abscent");
+                fullDayHour = 0;
             }
+            int  empWage = fullDayHour * wagePerHour;
+            Console.WriteLine("Daily wage is : " +empWage);
         }
     }
 }
