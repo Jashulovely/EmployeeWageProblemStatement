d2cc45179e5f121850c7ec002659bfe438c73ceb
jashulovely <porsu.jaswanthkumar@gmail.com>
2023-09-04 17:14:02 +0530

    [Jashu] Add: Employee Monthly Wage Calculation.

--- EmployeeWageProblem/EmployeeWageProblem/EmployeeWage.cs
+++ EmployeeWageProblem/EmployeeWageProblem/EmployeeWage.cs
@@ -8,29 +8,40 @@ namespace EmployeeWageProblem
 {
     internal class EmployeeWage
     {
-        const int wagePerHour = 20, isFullTime = 1, isPartTime = 2;
-        int fullDayHour = 0;
+        const int wagePerHour = 20, isFullTime = 1, isPartTime = 2, totalWorkingDays = 20;
+        int fullDayHour = 0, monthlyWage = 0;
+
+        Random random = new Random();
 
         public void Attendance()
         {
-            Random random = new Random();
-            int empCheck = random.Next(0,3);
+            for (int day = 1; day <= totalWorkingDays; day++) {
+                
+                int empCheck = random.Next(0, 3);
 
-            switch (empCheck)
-            {
-                case isFullTime: fullDayHour = 8;
-                    Console.WriteLine("Employee is present for Full Time.");
-                    break;
+                switch (empCheck)
+                {
+                    case isFullTime: fullDayHour = 8;
+                        Console.WriteLine(" Day {0} :-  Employee is present for Full Time.", day);
+                        break;
 
-                case isPartTime: fullDayHour = 4;
-                    Console.WriteLine("Employee is present for Part Time.");
-                    break;
-                default: fullDayHour = 0;
-                    Console.WriteLine("Employee is Abscent.");
-                    break;
+                    case isPartTime: fullDayHour = 4;
+                        Console.WriteLine(" Day {0} :-  Employee is present for Part Time.", day);
+                        break;
+                    default: fullDayHour = 0;
+                        Console.WriteLine(" Day {0} :-  Employee is Abscent.", day);
+                        break;
+                }
+                int empWage = fullDayHour * wagePerHour;
+                Console.WriteLine("      Daily wage of Employee is : " + empWage +"/-");
+                monthlyWage += empWage;
+
             }
-            int  empWage = fullDayHour * wagePerHour;
-            Console.WriteLine("Daily wage of Employee is : " + empWage);
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------------------------------------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("  Monthly Wages of the Employee is : " + monthlyWage +"/-");
         }
     }
 }
